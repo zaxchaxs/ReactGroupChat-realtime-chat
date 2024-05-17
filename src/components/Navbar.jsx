5cc15cfca4e0b8da9e261b526b2ca6309b7f3394
zaxchaxs <zaxchaxsecondid@gmail.com>
2024-05-17 15:13:30 +0700

    Creating Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable react/prop-types */
+
+export default function Navbar() {
+    return (
+      <div className="p-5 bg-sky-600 justify-between flex">
+        <div className="w-fit text-white font-bold text-center text-2xl">
+            <a href="/">ReactGroup Chat</a>
+        </div>
+        <div className="flex gap-8 items-center">
+            <Button val={"Home"} route={"/"} />
+            <Button val={"Chat"} route={"/chat"} />
+            <Button val={"About"} route={"/about"} />
+        </div>
+        <div className="text-white font-bold text-center text-xl items-center">
+            <Button isBtnLoggin={true} val={"Login"} route={"/Login"}/>
+        </div>
+      </div>
+    );
+}
+
+// child components
+function Button({ isBtnLoggin, val, route }) {
+    return (
+      <div className={`w-fit p-1 rounded-lg text-white font-bold text-center ${isBtnLoggin ? "bg-sky-700" : "bg-sky-500"} shadow-md hover:scale-105`}>
+        <a href={route} className="p-2 px-3">{val}</a>
+      </div>
+    );
+}

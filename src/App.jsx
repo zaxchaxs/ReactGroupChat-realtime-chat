5f22886ac10368c251f4d0c5039647b072f203ca
zaxchaxs <zaxchaxsecondid@gmail.com>
2024-06-12 21:51:48 +0700

    adding 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Homepage from './pages/Homepage';
 import ChatHomePage from "./pages/ChatHomePage";
 import ChatPage from "./pages/ChatPage";
 import AboutPage from "./pages/AboutPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
 
@@ -19,6 +20,7 @@ function App() {
             <Route path="/chat-homepage" element={<ChatHomePage />} />
             <Route path="/chat-page" element={<ChatPage />} />
             <Route path="/about" element={<AboutPage />} />
+            <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </AuthProvider>
       </Router>

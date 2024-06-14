e508d3c8869124e19533fbc5a0a0bdfe96a73a90
zaxchaxs <zaxchaxsecondid@gmail.com>
2024-06-14 11:30:14 +0700

    fix bug pwa

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    VitePWA({
-      manifest: {
-        name: "ReactGroup Chat",
-        short_name: "ReactGroup Chat",
-        description: "Simple web realtime chat with React.js",
-        theme_color: "#FFFFFF",
-        icons: [
-          {
-            src: 'favicon.svg',
-            sizes: '192x192',
-            type: 'image/svg+xml'
-          },
-          {
-            src: 'favicon.svg',
-            sizes: '512x512',
-            type: 'image/svg+xml'
-          }
-        ]
-      },
-      registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true
-      }
-    })
+    react()
   ],
 });
 
